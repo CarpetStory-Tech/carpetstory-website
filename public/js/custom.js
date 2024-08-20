b7a77e10e9dbcbf3821b32a56729bebb0da81916
CarpetStory <info@carpetstory.co>
2024-08-21 01:18:42 +0530

    removed google fonts

--- public/js/custom.js
+++ public/js/custom.js
@@ -424,7 +424,7 @@ $(function () {
     // Remove links that don't actually link to anything
     .not('[href="#"]').not('[href="#0"]').click(function (event) {
         // On-page links
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        if (window.location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && window.location.hostname == this.hostname) {
             // Figure out element to scroll to
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -451,37 +451,37 @@ $(function () {
     });
     
     //  Scroll back to top
-    var progressPath = document.querySelector('.progress-wrap path');
-    var pathLength = progressPath.getTotalLength();
-    progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
-    progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
-    progressPath.style.strokeDashoffset = pathLength;
-    progressPath.getBoundingClientRect();
-    progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
-    var updateProgress = function () {
-        var scroll = $(window).scrollTop();
-        var height = $(document).height() - $(window).height();
-        var progress = pathLength - (scroll * pathLength / height);
-        progressPath.style.strokeDashoffset = progress;
-    }
-    updateProgress();
-    $(window).scroll(updateProgress);
-    var offset = 150;
-    var duration = 550;
-    jQuery(window).on('scroll', function () {
-        if (jQuery(this).scrollTop() > offset) {
-            jQuery('.progress-wrap').addClass('active-progress');
-        } else {
-            jQuery('.progress-wrap').removeClass('active-progress');
-        }
-    });
-    jQuery('.progress-wrap').on('click', function (event) {
-        event.preventDefault();
-        jQuery('html, body').animate({
-            scrollTop: 0
-        }, duration);
-        return false;
-    })
+    // var progressPath = document.querySelector('.progress-wrap path');
+    // var pathLength = progressPath.getTotalLength();
+    // progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
+    // progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+    // progressPath.style.strokeDashoffset = pathLength;
+    // progressPath.getBoundingClientRect();
+    // progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+    // var updateProgress = function () {
+    //     var scroll = $(window).scrollTop();
+    //     var height = $(document).height() - $(window).height();
+    //     var progress = pathLength - (scroll * pathLength / height);
+    //     progressPath.style.strokeDashoffset = progress;
+    // }
+    // updateProgress();
+    // $(window).scroll(updateProgress);
+    // var offset = 150;
+    // var duration = 550;
+    // jQuery(window).on('scroll', function () {
+    //     if (jQuery(this).scrollTop() > offset) {
+    //         jQuery('.progress-wrap').addClass('active-progress');
+    //     } else {
+    //         jQuery('.progress-wrap').removeClass('active-progress');
+    //     }
+    // });
+    // jQuery('.progress-wrap').on('click', function (event) {
+    //     event.preventDefault();
+    //     jQuery('html, body').animate({
+    //         scrollTop: 0
+    //     }, duration);
+    //     return false;
+    // })
     
     
     // Select2
